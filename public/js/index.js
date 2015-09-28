75270e75cabf89022076e8e58d58491026d19ddd
ksami <ksami.ihide@gmail.com>
2015-09-29 01:11:13 +0800

    create rooms according to NUM_ROOMS, floor, hall wall, corridor, update docs

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 // Constants //
 ///////////////
 // 1m = 1 unit in any axis
-var SPEED_MOVE      = 20;       // m/s
+var SPEED_MOVE      = 10;       // m/s
 var SPEED_TURN      = Math.PI;  // rad/s
 var LENGTH_HALL     = 100;
 var BREADTH_HALL    = 65;
@@ -40,7 +40,7 @@ function init(){
 
     // Camera
     //args: fov, aspect ratio, near, far
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1100);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 2000);
 
 
     // Renderer
@@ -70,11 +70,11 @@ function init(){
     scene.add(ambientLight);
 
 
-    // Cube
+    // Person
     let cubeGeometry = new THREE.BoxGeometry(2,1,1);
     let cubeMaterial = new THREE.MeshPhongMaterial();
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(25,1,25);
+    cube.position.set(12,1,12);
     cube.rotation.y = Math.PI/2;
     scene.add(cube);
 
@@ -85,38 +85,34 @@ function init(){
 
 
     // Rooms
-    let room = createRoom(new THREE.Vector3(0,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
-    scene.add(room);
-    let room2 = createRoom(new THREE.Vector3(LENGTH_ROOM,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
-    scene.add(room2);
-    collidableMeshList = collidableMeshList.concat(getMeshes(room));
-    collidableMeshList = collidableMeshList.concat(getMeshes(room2));
+    for(let i=0; i<NUM_ROOMS/2; i++){
+        //create 2 rows with corridor in between
+        let room = createRoom(new THREE.Vector3(i*LENGTH_ROOM,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
+        scene.add(room);
+        collidableMeshList = collidableMeshList.concat(getMeshes(room));
+
+        let room2 = createRoom(new THREE.Vector3(i*LENGTH_ROOM,0,BREADTH_ROOM+BREADTH_CORRIDOR), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
+        scene.add(room2);
+        collidableMeshList = collidableMeshList.concat(getMeshes(room2));
+    }
 
 
     // Hall Floor
-    var floorGeometry = new THREE.PlaneBufferGeometry(LENGTH_HALL, BREADTH_HALL);
-    var floorMaterial = new THREE.MeshBasicMaterial({color: 0x444444});
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    let floorGeometry = new THREE.PlaneBufferGeometry(LENGTH_HALL, BREADTH_HALL);
+    let floorMaterial = new THREE.MeshBasicMaterial({color: 0x444444});
+    let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.position.set(LENGTH_HALL/2, 0, BREADTH_HALL/2);
     floor.rotation.x = -Math.PI/2;
     scene.add(floor);
 
-    //TODO: add corridor
-
-/*
     // Hall Walls
-    drawRoom(new THREE.Vector3(0,0,0), LENGTH_HALL, HEIGHT_HALL, BREADTH_HALL);
+    let hall = createRoom(new THREE.Vector3(0,0,0), LENGTH_HALL, HEIGHT_HALL, BREADTH_HALL, {light: false});
+    scene.add(hall);
+    collidableMeshList = collidableMeshList.concat(getMeshes(hall));
 
-    // Rooms
-    for (var i = (-NUM_ROOMS/2)+1; i < NUM_ROOMS/2; i+=2){
-        var centre = new THREE.Vector3((i*LENGTH_ROOM)/2, 0, ((BREADTH_HALL/2) - (BREADTH_ROOM/2)));
-        drawRoom(centre, LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
-        centre.z = -((BREADTH_HALL/2) - (BREADTH_ROOM/2));
-        drawRoom(centre, LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
-    }
-*/
 
     // Skybox
-    let skyBoxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+    let skyBoxGeometry = new THREE.BoxGeometry(3000, 1000, 1000);
     let skyBoxMaterial = new THREE.MeshBasicMaterial({color: 0x9999ff, side: THREE.BackSide});
     let skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
     scene.add(skyBox);
@@ -247,9 +243,14 @@ function getMeshes(obj){
  * @param  {number} length - Length of the room in the x-axis
  * @param  {number} height - Height of the room in the y-axis
  * @param  {number} breadth - Breadth of the walls in the z-axis
+ * @param {Object=} opts - Options for creating the room
+ * @param {boolean} [opts.hasLight=true] - If true, creates a main light in the center of the room
+ * @param {boolean} [opts.hasDoor=false] - If true, creates a door for the room
  * @return  {THREE.Object3D} room - Dummy room object to group walls and light
  */
-function createRoom(corner, length, height, breadth){
+function createRoom(corner, length, height, breadth, opts){
+    opts = opts || {light: true, door: false};
+
     //TODO: leave space for door!
     let x = corner.x;
     let y = corner.y;
@@ -258,10 +259,12 @@ function createRoom(corner, length, height, breadth){
 
     let room = new THREE.Object3D();
 
-    // Light in the centre of the room
-    let pointLight = new THREE.PointLight(0x999999, 5, length/4*3);
-    pointLight.position.set(x+length/2,y+20,z+breadth/2);
-    room.add(pointLight);
+    if(opts.hasOwnProperty('light') && opts['light']){
+        // Light in the centre of the room
+        let pointLight = new THREE.PointLight(0x999999, 5, length/4*3);
+        pointLight.position.set(x+length/2,y+20,z+breadth/2);
+        room.add(pointLight);
+    }
 
 
     let wallLengthGeometry = new THREE.PlaneBufferGeometry(length, height);
@@ -316,8 +319,8 @@ socket.on('eventDetails', function(event){
     // constrain number of rooms to even number
     let num = parseInt(event.num);
     NUM_ROOMS = (num%2) ? num+1 : num;
-    LENGTH_HALL = LENGTH_ROOM * NUM_ROOMS;
-    BREADTH_HALL = (BREADTH_ROOM * NUM_ROOMS * 2) + BREADTH_CORRIDOR;
+    LENGTH_HALL = LENGTH_ROOM * (NUM_ROOMS / 2);
+    BREADTH_HALL = (BREADTH_ROOM * 2) + BREADTH_CORRIDOR;
     LENGTH_CORRIDOR = LENGTH_HALL;
 
     /////////////
