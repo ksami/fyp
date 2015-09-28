630b22cfec2905e773846ab25c93eed1e06613b8
ksami <ksami.ihide@gmail.com>
2015-09-29 00:37:18 +0800

    convert var to let, add rooms to collidables

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,16 @@
 // 1m = 1 unit in any axis
 var SPEED_MOVE      = 20;       // m/s
 var SPEED_TURN      = Math.PI;  // rad/s
-var LENGTH_HALL     = 300;      // m
-var BREADTH_HALL    = 150;      // m
-var HEIGHT_HALL     = 10;        // m
-var NUM_ROOMS       = 4;        // default value
-var LENGTH_ROOM     = 100;
-var BREADTH_ROOM    = 50;
-var HEIGHT_ROOM     = 9;        // m
+var LENGTH_HALL     = 100;
+var BREADTH_HALL    = 65;
+var HEIGHT_HALL     = 10;
+var NUM_ROOMS       = 4;
+var LENGTH_ROOM     = 50;
+var BREADTH_ROOM    = 25;
+var HEIGHT_ROOM     = 10;
+var LENGTH_CORRIDOR = LENGTH_HALL;
+var BREADTH_CORRIDOR= 15;
+var HEIGHT_CORRIDOR = 10; 
 
 
 /////////////
@@ -31,7 +34,7 @@ function init(){
     scene = new THREE.Scene();
 
     //debug: Axis
-    var axisHelper = new THREE.AxisHelper(3);
+    let axisHelper = new THREE.AxisHelper(3);
     scene.add(axisHelper);
 
 
@@ -63,13 +66,13 @@ function init(){
 
 
     // Lights
-    var ambientLight = new THREE.AmbientLight(0xa0a0a0);
+    let ambientLight = new THREE.AmbientLight(0xa0a0a0);
     scene.add(ambientLight);
 
 
     // Cube
-    var cubeGeometry = new THREE.BoxGeometry(2,1,1);
-    var cubeMaterial = new THREE.MeshPhongMaterial();
+    let cubeGeometry = new THREE.BoxGeometry(2,1,1);
+    let cubeMaterial = new THREE.MeshPhongMaterial();
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.set(25,1,25);
     cube.rotation.y = Math.PI/2;
@@ -82,20 +85,24 @@ function init(){
 
 
     // Rooms
-    var room = createRoom(new THREE.Vector3(0,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
+    let room = createRoom(new THREE.Vector3(0,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
     scene.add(room);
-    // let room2 = createRoom(new THREE.Vector3(0, 0, LENGTH_ROOM), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
-    // scene.add(room2);
+    let room2 = createRoom(new THREE.Vector3(LENGTH_ROOM,0,0), LENGTH_ROOM, HEIGHT_ROOM, BREADTH_ROOM);
+    scene.add(room2);
+    collidableMeshList = collidableMeshList.concat(getMeshes(room));
+    collidableMeshList = collidableMeshList.concat(getMeshes(room2));
 
 
-/*
     // Hall Floor
-    var floorGeometry = new THREE.PlaneGeometry(LENGTH_HALL, BREADTH_HALL);
+    var floorGeometry = new THREE.PlaneBufferGeometry(LENGTH_HALL, BREADTH_HALL);
     var floorMaterial = new THREE.MeshBasicMaterial({color: 0x444444});
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = -Math.PI/2;
     scene.add(floor);
 
+    //TODO: add corridor
+
+/*
     // Hall Walls
     drawRoom(new THREE.Vector3(0,0,0), LENGTH_HALL, HEIGHT_HALL, BREADTH_HALL);
 
@@ -109,9 +116,9 @@ function init(){
 */
 
     // Skybox
-    var skyBoxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
-    var skyBoxMaterial = new THREE.MeshBasicMaterial({color: 0x9999ff, side: THREE.BackSide});
-    var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
+    let skyBoxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+    let skyBoxMaterial = new THREE.MeshBasicMaterial({color: 0x9999ff, side: THREE.BackSide});
+    let skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
     scene.add(skyBox);
 
     // Load model
@@ -142,14 +149,17 @@ function render(){
     renderer.render(scene, camera);
 }
 
+/**
+ * Update state and handle keyboard input
+ */
 function update(){
 
     // delta = change in time since last call (seconds)
-    var delta = clock.getDelta(); 
-    var moveDistance = SPEED_MOVE * delta;
-    var turnArc = SPEED_TURN * delta;
+    let delta = clock.getDelta(); 
+    let moveDistance = SPEED_MOVE * delta;
+    let turnArc = SPEED_TURN * delta;
 
-    var collisionResult = detectCollision(cube, collidableMeshList);
+    let collisionResult = detectCollision(cube, collidableMeshList);
    
     // move forwards/backwards
     if(keyboard.pressed('w')){
@@ -176,30 +186,30 @@ function update(){
 }
 
 /**
- * Detects if there is a collision between obj and a list
- * @param  {THREE.Mesh} obj - Object to test collisions on
+ * Detects if there is a collision between msh and a list
+ * @param  {THREE.Mesh} msh - Object to test collisions on
  * @param  {THREE.Mesh[]} collidableMeshList - Array of meshes to test collision against
  * @return  {Object} result - Result of collision
  * @return  {boolean} result.isCollided - true if collision occurred
  * @return  {string} result.sideToBlock - Whether collision occurred on 'front', 'back' or 'none'
  */
-function detectCollision(obj, collidableMeshList){
+function detectCollision(msh, collidableMeshList){
     // collision detection:
     //   determines if any of the rays from the object's origin to each vertex
     //      intersects any face of a mesh in the array of target meshes
     //   for increased collision accuracy, add more vertices to the cube;
     //      for example, new THREE.CubeGeometry( 64, 64, 64, 8, 8, 8, wireMaterial )
     //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
-    var originPoint = obj.position.clone();
-    //todo: use bounding box of obj
+    let originPoint = msh.position.clone();
+    //todo: use bounding box of msh
     
-    for (var vertexIndex = 0; vertexIndex < obj.geometry.vertices.length; vertexIndex++){       
-        var localVertex = obj.geometry.vertices[vertexIndex].clone();
-        var globalVertex = localVertex.applyMatrix4(obj.matrix);
-        var directionVector = globalVertex.sub(obj.position);
+    for (let vertexIndex = 0; vertexIndex < msh.geometry.vertices.length; vertexIndex++){       
+        let localVertex = msh.geometry.vertices[vertexIndex].clone();
+        let globalVertex = localVertex.applyMatrix4(msh.matrix);
+        let directionVector = globalVertex.sub(msh.position);
         
-        var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-        var collisionResults = ray.intersectObjects(collidableMeshList);
+        let ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+        let collisionResults = ray.intersectObjects(collidableMeshList);
 
         if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()){
             //collision for this ray from
@@ -215,6 +225,21 @@ function detectCollision(obj, collidableMeshList){
     return {isCollided: false, sideToBlock: 'none'};
 }
 
+/**
+ * Get child meshes from an Object3D
+ * @param  {THREE.Object3D} obj - Parent object with child meshes
+ * @return {THREE.Mesh[]} arr - Array of child meshes
+ */
+function getMeshes(obj){
+    let arr = [];
+    obj.traverse(function(node){
+        if(node instanceof THREE.Mesh){
+            arr.push(node);
+        }
+    });
+    return arr;
+}
+
 
 /**
  * Creates 4 walls and a point light
@@ -222,44 +247,46 @@ function detectCollision(obj, collidableMeshList){
  * @param  {number} length - Length of the room in the x-axis
  * @param  {number} height - Height of the room in the y-axis
  * @param  {number} breadth - Breadth of the walls in the z-axis
- * @return {THREE.Object3D} room - Dummy room object to group walls and light
+ * @return  {THREE.Object3D} room - Dummy room object to group walls and light
  */
 function createRoom(corner, length, height, breadth){
     //TODO: leave space for door!
-    var x = corner.x;
-    var y = corner.y;
-    var z = corner.z;
+    let x = corner.x;
+    let y = corner.y;
+    let z = corner.z;
 
 
-    var room = new THREE.Object3D();
+    let room = new THREE.Object3D();
 
     // Light in the centre of the room
-    var pointLight = new THREE.PointLight(0x999999, 5, 75);
+    let pointLight = new THREE.PointLight(0x999999, 5, length/4*3);
     pointLight.position.set(x+length/2,y+20,z+breadth/2);
     room.add(pointLight);
 
-    var wallLengthGeometry = new THREE.PlaneBufferGeometry(length, height);
-    var wallBreadthGeometry = new THREE.PlaneBufferGeometry(breadth, height);
-    var wallMaterial = new THREE.MeshPhongMaterial({color: 0x9999999, side: THREE.DoubleSide});
-    var wallLength1 = new THREE.Mesh(wallLengthGeometry, wallMaterial);
-    var wallLength2 = new THREE.Mesh(wallLengthGeometry, wallMaterial);
-    var wallBreadth1 = new THREE.Mesh(wallBreadthGeometry, wallMaterial);
-    var wallBreadth2 = new THREE.Mesh(wallBreadthGeometry, wallMaterial);
+
+    let wallLengthGeometry = new THREE.PlaneBufferGeometry(length, height);
+    let wallBreadthGeometry = new THREE.PlaneBufferGeometry(breadth, height);
+    let wallMaterial = new THREE.MeshPhongMaterial({color: 0x9999999, side: THREE.DoubleSide});
+    let wallLength1 = new THREE.Mesh(wallLengthGeometry, wallMaterial);
+    let wallLength2 = new THREE.Mesh(wallLengthGeometry, wallMaterial);
+    let wallBreadth1 = new THREE.Mesh(wallBreadthGeometry, wallMaterial);
+    let wallBreadth2 = new THREE.Mesh(wallBreadthGeometry, wallMaterial);
+
     //set corner at (0,0,0)
     wallLength1.position.set(x+length/2, y+height/2, z);
     wallLength2.position.set(x+length/2, y+height/2, z+breadth);
     wallBreadth1.position.set(x, y+height/2, z+breadth/2);
     wallBreadth2.position.set(x+length, y+height/2, z+breadth/2);
+
     //rotate about own origin
     wallBreadth1.rotation.y = Math.PI/2;
     wallBreadth2.rotation.y = Math.PI/2;
-    // wallBreadth1.rotation.y = Math.PI/2;
+
     room.add(wallLength1);
     room.add(wallLength2);
     room.add(wallBreadth1);
     room.add(wallBreadth2);
 
-    //TODO: add to collidables
 
     return room;
 }
@@ -287,10 +314,11 @@ socket.on('eventDetails', function(event){
     document.getElementById('loading').innerHTML = '';
 
     // constrain number of rooms to even number
-    var num = parseInt(event.num);
+    let num = parseInt(event.num);
     NUM_ROOMS = (num%2) ? num+1 : num;
-    // LENGTH_ROOM = LENGTH_HALL/(NUM_ROOMS/2);
-    // BREADTH_ROOM = BREADTH_HALL/3;
+    LENGTH_HALL = LENGTH_ROOM * NUM_ROOMS;
+    BREADTH_HALL = (BREADTH_ROOM * NUM_ROOMS * 2) + BREADTH_CORRIDOR;
+    LENGTH_CORRIDOR = LENGTH_HALL;
 
     /////////////
     // Execute //
